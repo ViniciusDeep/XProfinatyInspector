6f1efa1cffae86451b701dbb96ee52921e48ba79
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-11-15 18:57:04 -0300

    Fix problems to identify bad word

--- XProfinatyInspector/Source/XProfinatyInspector.swift
+++ XProfinatyInspector/Source/XProfinatyInspector.swift
@@ -8,118 +8,58 @@
 
 import Foundation
 
-class XProfinatyResource {
-    static func fileURL() -> URL? {
-        return Bundle(for: XProfinatyResource.self).url(forResource: "Profanity", withExtension: "json")
+class ProfanityResources {
+    
+    class func profanityFileURL() -> URL? {
+        return Bundle(for: ProfanityResources.self).url(forResource: "profanity", withExtension: "json")
     }
 }
 
-struct XProfanityWords {
+struct ProfanityDictionary {
     
-    static let profaneWords: Set<String> = {
+    static let profaneWords: [String] = {
         
-        guard let fileURL = XProfinatyResource.fileURL() else {
-            return Set<String>()
+        guard let fileURL = ProfanityResources.profanityFileURL() else {
+            return []
         }
-        
+    
         do {
             let fileData = try Data(contentsOf: fileURL, options: NSData.ReadingOptions.uncached)
             
             guard let words = try JSONSerialization.jsonObject(with: fileData, options: []) as? [String] else {
-                return Set<String>()
+                return []
             }
             
-            return Set(words)
+            return words
             
         } catch {
-            return Set<String>()
+            return []
         }
     }()
 }
 
-public struct ProfanityFilter {
-    
-    static func censorString(_ string: String) -> String {
-        var cleanString = string
-        
-        for word in string.profaneWords() {
-            
-            let cleanWord = "".padding(toLength: word.count, withPad: "*", startingAt: 0)
-            
-            cleanString = cleanString.replacingOccurrences(of: word, with: cleanWord, options: [.caseInsensitive], range: nil)
-        }
-        
-        return cleanString
-    }
-}
-
 public extension String {
     
-    func profaneWords() -> Set<String> {
+ 
+    func containsProfanity() -> Bool {
+        
+        var hasBadWords = false
         
         var delimiterSet = CharacterSet()
-        delimiterSet.formUnion(CharacterSet.punctuationCharacters)
-        delimiterSet.formUnion(CharacterSet.whitespacesAndNewlines)
+         delimiterSet.formUnion(CharacterSet.punctuationCharacters)
+         delimiterSet.formUnion(CharacterSet.whitespacesAndNewlines)
+         
+         let words = Set(self.lowercased().components(separatedBy: delimiterSet))
         
-        let words = Set(self.lowercased().components(separatedBy: delimiterSet))
-        
-        return words.intersection(XProfanityWords.profaneWords)
-    }
-    
-    func containsProfanity() -> Bool {
-        return !profaneWords().isEmpty
-    }
-    
-    func censored() -> String {
-        return ProfanityFilter.censorString(self)
-    }
-    
-    mutating func censor() {
-        self = censored()
-    }
-}
-
-public extension NSString {
-    
-    func censored() -> NSString {
-        return ProfanityFilter.censorString(self as String) as NSString
-    }
-}
-
-public extension NSMutableString {
-    
-    func censor() {
-        setString(ProfanityFilter.censorString(self as String))
-    }
-}
-
-public extension NSAttributedString {
-    
-    func censored() -> NSAttributedString {
-        
-        let profaneWords = string.profaneWords()
-        
-        if profaneWords.isEmpty {
-            return self
-        }
-        
-        let cleanString = NSMutableAttributedString(attributedString: self)
-        
-        for word in profaneWords {
-            
-            let cleanWord = "".padding(toLength: word.count, withPad: "*", startingAt: 0)
-            
-            var range = (cleanString.string as NSString).range(of: word, options: .caseInsensitive)
-            while range.location != NSNotFound {
-                let attributes = cleanString.attributes(at: range.location, effectiveRange: nil)
-                let cleanAttributedString = NSAttributedString(string: cleanWord, attributes: attributes)
-                cleanString.replaceCharacters(in: range, with: cleanAttributedString)
-                
-                range = (cleanString.string as NSString).range(of: word, options: .caseInsensitive)
+        ProfanityDictionary.profaneWords.forEach { (badWord) in
+            if words.contains(badWord) {
+                hasBadWords = true
+            } else {
+                hasBadWords = false
             }
         }
         
-        return cleanString
+        return hasBadWords
     }
 }
 
